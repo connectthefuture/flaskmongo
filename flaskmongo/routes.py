01c231a141655d2044a21f5c3286bdea693f981a
johnb <john.bragato@gmail.com>
2015-01-10 03:32:29 -0500

    x

--- flaskmongo/routes.py
+++ flaskmongo/routes.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask import Flask
+app = Flask(__name__)
+
 
 @app.route('/')
 def home_page():
@@ -39,9 +42,6 @@ def show_post(post_id):
     return 'Post %d' % post_id
 
 
-
-from flask import Flask
-app = Flask(__name__)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
     
