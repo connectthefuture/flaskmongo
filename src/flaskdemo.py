e383cceb750bab68de367fe001835760bcd5654b
johnb <john.bragato@gmail.com>
2015-01-16 12:42:41 -0500

    x

--- src/flaskdemo.py
+++ src/flaskdemo.py
@@ -16,6 +16,44 @@ def index():
 def echo():
     ret_data = {"value": request.args.get('echoValue')}
     return jsonify(ret_data)
- 
+
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+
+    elif request.method == 'PATCH':
+        return "ECHO: PACTH\n"
+
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE"
+
+from flask import json
+
+@app.route('/images', methods = ['POST'])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+
+    else:
+        return "415 Unsupported Media Type ;)"
+
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
+
+
