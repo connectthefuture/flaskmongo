2faea1e079e82a9969eb2a13221bafc4ae670981
johnb <john.bragato@gmail.com>
2015-04-26 20:27:26 -0400

    x

--- src/my_mongo/app.py
+++ src/my_mongo/app.py
@@ -2,7 +2,7 @@ __author__ = 'johnb'
 
 from flask import Flask
 
-# App
+
 app = Flask(
     __name__,
     static_url_path = '/static',
@@ -11,11 +11,11 @@ app = Flask(
 app.config.from_object('my_mongo.config')
 
 ## DB
-import flask-mongoengine
-from redis import Redis
+from flask_mongoengine import MongoEngine
+#from redis import Redis
 app.config['MONGODB_SETTINGS'] = {'DB': 'images'}
-db = flask-mongoengine.MongoEngine(app)
-redis = Redis()
+db = MongoEngine(app)
+#redis = Redis()
 
 # Model views
 from my_mongo.product.views import image
